b255dcc7f67918d2255d3bb02c9469021701d10c
indrih17 <indrih17@gmail.com>
2019-11-27 01:09:03 +0300

    Version 0.1.11: * Deleted lock files. * Updated podspec and podfile.

--- sharedCode/build.gradle.kts
+++ sharedCode/build.gradle.kts
@@ -33,7 +33,7 @@ kotlin {
     }
 
     // Cocoa pods
-    version = "0.1.10"
+    version = "0.1.11"
     cocoapods {
         // Configure fields required by CocoaPods.
         summary = "Kubsu timetable"
